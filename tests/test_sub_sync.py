d5a319159cbcc88def1f64f886f9c7afb69d87d5
Ye Jun <yejun.code@gmail.com>
2022-12-01 14:26:34 +0800

    update Dockerfile && update build tools

--- tests/test_sub_sync.py
+++ tests/test_sub_sync.py
@@ -1,5 +1,13 @@
 from sub_sync import __version__
+import toml
+from pathlib import Path
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    path = Path(__file__).resolve().parents[1] / "pyproject.toml"
+    pyproject = toml.loads(open(str(path)).read())
+    pyproject_version = pyproject["tool"]["poetry"]["version"]
+
+    package_init_version = __version__
+
+    assert package_init_version == pyproject_version
